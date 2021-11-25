a09107d116bc88b7374d936baffc28f91ec4a50c
pinscelebs <92086656+pinscelebs@users.noreply.github.com>
2021-11-25 16:02:42 +0700

    Update smartlink.js

--- smartlink.js
+++ smartlink.js
@@ -1,2 +1,8 @@
-/* smartlink https://bit.ly/revadav */
+/* 
+smartlink     : for celeb
+official site : https://bit.ly/revadav 
+license       : free join
+version       : 0.1
+supported     : https://vk.aridjaya.com
+*/
 window.location.href = 'https://cqwajn.com/gosl/InNpZCI6MTA3NDQ4Nywic21hcnRsaW5rIjp0cnVlfQ==eyJwaWQiOjEwMjg3Nzcs?si1=blogspot&si2=celeb';
